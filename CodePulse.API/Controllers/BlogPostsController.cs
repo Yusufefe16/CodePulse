5e55374f5217c59c0aef3486213221b7436d0e98
Yusuf <yefe1538@gmail.com>
2024-11-11 23:16:25 +0300

    relationship in entityframework between blogposts and categories

--- CodePulse.API/Controllers/BlogPostsController.cs
+++ CodePulse.API/Controllers/BlogPostsController.cs
@@ -1,65 +1,56 @@
 ﻿using CodePulse.API.Models.Domain;
 using CodePulse.API.Models.DTO;
 using CodePulse.API.Repositories.Interface;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
-
-namespace CodePulse.API.Controllers;
-
-[Route("/api/[controller]")]
-[ApiController]
-public class BlogPostsController : Controller
+namespace CodePulse.API.Controllers
 {
-    private readonly IBlogPostRepository blogPostRepository;
-    
-    public BlogPostsController(IBlogPostRepository BlogPostRepository)
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BlogPostsController : ControllerBase
     {
-        this.blogPostRepository = BlogPostRepository;
-    }
-    
-    
-    [HttpPost]
-    public async Task<IActionResult> CreateBlogPost([FromBody] CreateBlogPostRequestDto request)
-    {
-        var blogPost = new BlogPost
+        private readonly IBlogPostRepository blogPostRepository;
+        private readonly ICategoryRepository categoryRepository;
+
+        public BlogPostsController(IBlogPostRepository blogPostRepository, 
+            ICategoryRepository categoryRepository)
         {
-            Author = request.Author,
-            Content = request.Content,
-            FeaturedImageUrl = request.FeaturedImageUrl,
-            IsVisible = request.IsVisible,
-            PublishedDate = request.PublishedDate,
-            ShortDescription = request.ShortDescription,
-            Title = request.Title,
-            UrlHandle = request.UrlHandle,
-        };
-        
-        blogPost = await blogPostRepository.CreateAsync(blogPost);
+            this.blogPostRepository = blogPostRepository;
+            this.categoryRepository = categoryRepository;
+        }
 
-        var response = new BlogPostDto
+        // POST: {apibaseurl}/api/blogposts
+        [HttpPost]
+        public async Task<IActionResult> CreateBlogPost([FromBody] CreateBlogPostRequestDto request)
         {
-            Id = blogPost.Id,
-            Author = blogPost.Author,
-            Content = blogPost.Content,
-            FeaturedImageUrl = blogPost.FeaturedImageUrl,
-            IsVisible = blogPost.IsVisible,
-            PublishedDate = blogPost.PublishedDate,
-            ShortDescription = blogPost.ShortDescription,
-            Title = blogPost.Title,
-            UrlHandle = blogPost.UrlHandle
-        };
-        
-        return Ok(response);
+            // Convert DTO to DOmain
+            var blogPost = new BlogPost
+            {
+                Author = request.Author,
+                Content = request.Content,
+                FeaturedImageUrl = request.FeaturedImageUrl,
+                IsVisible = request.IsVisible,
+                PublishedDate = request.PublishedDate,
+                ShortDescription = request.ShortDescription,
+                Title = request.Title,
+                UrlHandle = request.UrlHandle,
+                Categories = new List<Category>()
+            };
 
-    }
 
-    [HttpGet]
-    public async Task<IActionResult> GetAllBlogPosts()
-    {
-        var blogPosts = await blogPostRepository.GetAllAsync();
+            foreach (var categoryGuid in request.Categories)
+            {
+                var existingCategory = await categoryRepository.GetById(categoryGuid);
+                if (existingCategory is not null)
+                {
+                    blogPost.Categories.Add(existingCategory);
+                }
+            }
 
-        var response = new List<BlogPostDto>();
-        foreach (var blogPost in blogPosts)
-        {
-            response.Add(new BlogPostDto
+            blogPost = await blogPostRepository.CreateAsync(blogPost);
+
+            // Convert Domain Model back to DTO
+            var response = new BlogPostDto
             {
                 Id = blogPost.Id,
                 Author = blogPost.Author,
@@ -69,11 +60,47 @@ public class BlogPostsController : Controller
                 PublishedDate = blogPost.PublishedDate,
                 ShortDescription = blogPost.ShortDescription,
                 Title = blogPost.Title,
-                UrlHandle = blogPost.UrlHandle
-            });
+                UrlHandle = blogPost.UrlHandle,
+                Categories = blogPost.Categories.Select(x => new CategoryDto
+                {
+                    Id = x.Id,
+                    Name = x.Name,
+                    UrlHandle = x.UrlHandle
+                }).ToList()
+            };
+
+            return Ok(response);
         }
-        return Ok(response);
+        // GET: {apibaseurl}/api/blogposts
+        [HttpGet]
+        public async Task<IActionResult> GetAllBlogPosts()
+        {
+            var blogPosts = await blogPostRepository.GetAllAsync();
+            // Convert Domain model to DTO
+            var response = new List<BlogPostDto>();
+            foreach (var blogPost in blogPosts)
+            {
+                response.Add(new BlogPostDto
+                {
+                    Id = blogPost.Id,
+                    Author = blogPost.Author,
+                    Content = blogPost.Content,
+                    FeaturedImageUrl = blogPost.FeaturedImageUrl,
+                    IsVisible = blogPost.IsVisible,
+                    PublishedDate = blogPost.PublishedDate,
+                    ShortDescription = blogPost.ShortDescription,
+                    Title = blogPost.Title,
+                    UrlHandle = blogPost.UrlHandle,
+                    Categories = blogPost.Categories.Select(x => new CategoryDto
+                    {
+                        Id = x.Id,
+                        Name = x.Name,
+                        UrlHandle = x.UrlHandle
+                    }).ToList()
+                });
+            }
+
+            return Ok(response);
+        }
     }
-    
-    
 }
